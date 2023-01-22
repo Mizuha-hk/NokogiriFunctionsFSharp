d011e9df91501c4460e01d268911ea77436dfcb6
Mizuha-hk <108075796+Mizuha-hk@users.noreply.github.com>
2023-01-22 14:11:01 +0900

    change authorizationLevel

--- HttpTriggerFSharp/HttpTriggerFSharp.fs
+++ HttpTriggerFSharp/HttpTriggerFSharp.fs
@@ -1,13 +1,17 @@
 namespace Company.Function
 
 open System
+open System.IO
 open System.Collections.Generic
+open System.Threading.Tasks
 open Microsoft.AspNetCore.Mvc
 open Microsoft.Azure.WebJobs
 open Microsoft.Azure.WebJobs.Extensions.Http
 open Microsoft.AspNetCore.Http
 open Microsoft.Extensions.Logging
+open Newtonsoft.Json
 open NameAndAid
+
 
         
 
@@ -15,7 +19,7 @@ open NameAndAid
 module Functions =
     //roomId‚©‚çƒ‹[ƒ€î•ñ‚ð•Ô‚· (room -> [{room,id1,id2,isStreaming}])
     [<FunctionName("GetRoomInfo")>]
-    let GetRoomInfoRun ([<HttpTrigger(AuthorizationLevel.Function, "get", Route = "getRoomInfo/{roomid}")>]req: HttpRequest) ([<Sql("select * from dbo.[Room] where Room = @Room", CommandType = System.Data.CommandType.Text, Parameters = "@Room={roomid}", ConnectionStringSetting = "ConnectionSetting")>]data: IEnumerable<RoomIdStreamingStatus>) (log: ILogger) =
+    let GetRoomInfoRun ([<HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "getRoomInfo/{roomid}")>]req: HttpRequest) ([<Sql("select * from dbo.[Room] where Room = @Room", CommandType = System.Data.CommandType.Text, Parameters = "@Room={roomid}", ConnectionStringSetting = "ConnectionSetting")>]data: IEnumerable<RoomIdStreamingStatus>) (log: ILogger) =
         async {
             log.LogInformation("Get RoomInfo Function processed a request.")
 
@@ -24,7 +28,7 @@ module Functions =
 
     //player‚ÌId‚©‚ç‰ž‰‡Point‚ð•Ô‚· (id -> [{"id","point"}])
     [<FunctionName("GetPoint")>]
-    let GetPointRun ([<HttpTrigger(AuthorizationLevel.Function, "get", Route = "getPoint/{id}")>]req: HttpRequest) ([<Sql("select * from dbo.[Point] where Id = @Id", CommandType = System.Data.CommandType.Text, Parameters = "@Id={id}", ConnectionStringSetting = "ConnectionSetting")>]idandpoint: IEnumerable<IdAndPoint>) (log: ILogger) =
+    let GetPointRun ([<HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "getPoint/{id}")>]req: HttpRequest) ([<Sql("select * from dbo.[Point] where Id = @Id", CommandType = System.Data.CommandType.Text, Parameters = "@Id={id}", ConnectionStringSetting = "ConnectionSetting")>]idandpoint: IEnumerable<IdAndPoint>) (log: ILogger) =
         async {
             log.LogInformation("Get Point Function processed a request")
 
@@ -34,7 +38,7 @@ module Functions =
 
     //ˆêˆÓ‚ÌroomId‚ð•Ô‚·Function (null->string)
     [<FunctionName("GetRoomNum")>]
-    let GetRoomNumRun([<HttpTrigger(AuthorizationLevel.Function,"get", Route = "GetRoomNum")>]req: HttpRequest) (log: ILogger) =
+    let GetRoomNumRun([<HttpTrigger(AuthorizationLevel.Anonymous,"get", Route = "GetRoomNum")>]req: HttpRequest) (log: ILogger) =
         async {
             log.LogInformation("Get Room Num processed a request.")
             
@@ -43,5 +47,4 @@ module Functions =
                                
             return OkObjectResult(NewRoomId) :>IActionResult
         } |> Async.StartAsTask
-    
-
+   
